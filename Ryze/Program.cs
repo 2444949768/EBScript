cef72dc3c08b069abb9e9526012d15c3d78ccaf1
samuelchow1997 <samuelchow1997@gmail.com>
2017-08-05 20:40:41 +0800

    release
    release

---
+++ Ryze/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Lwan_VIPSeries
+{
+    using EloBuddy;
+    using EloBuddy.Sandbox;
+    using EloBuddy.SDK.Events;
+
+    using System;
+    using System.Drawing;
+    using System.IO;
+    using System.Reflection;
+
+    internal class Program
+    {
+        private static readonly string dllPath = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\EloBuddy\Addons\Libraries\AR.dll";
+
+        private static void Main(string[] Args)
+        {
+            Loading.OnLoadingComplete += eventArgs =>
+            {
+                if (File.Exists(dllPath))
+                {
+                    File.Delete(dllPath);
+                }
+
+                var bydll = Properties.Resources.AR;
+                using (var fs = new FileStream(dllPath, FileMode.Create))
+                {
+                    fs.Write(bydll, 0, bydll.Length);
+                }
+
+                var dllpath = Assembly.LoadFrom(dllPath);
+                var main = dllpath.GetType("d").GetMethod("e", BindingFlags.NonPublic | BindingFlags.Static);
+
+                main.Invoke(null, null);
+                
+            };
+        }
+    }
+}
